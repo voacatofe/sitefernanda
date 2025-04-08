80213263d42ea9f9de5e3360466846ac7272fe59
Developer <user@example.com>
2025-04-08 10:21:24 -0300

    fix: voltar para o modo standalone e adicionar configurações de servidor

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  distDir: 'out',
-  images: {
-    unoptimized: true,
-  },
+  output: 'standalone',
   serverExternalPackages: ['@prisma/client', 'bcryptjs']
 }
 
