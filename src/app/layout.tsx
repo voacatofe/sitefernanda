2e908a4eecac7293155fb424ce9afc58c023f718
Developer <user@example.com>
2025-04-14 12:11:44 -0300

    feat: adiciona scripts Google Tag Manager e RD Station

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,7 @@ import { galano } from "./fonts"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { AuthProvider } from "@/components/auth-provider"
-import Header from "@/components/header"
-import Footer from "@/components/footer"
+import Script from "next/script"
 
 export const metadata: Metadata = {
   title: "Fernanda | Consultora Imobiliária Dimas Construções",
@@ -21,12 +20,38 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR" suppressHydrationWarning>
+      {/* Google Tag Manager Script */}
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-M9DWRX7N');
+        `}
+      </Script>
       <body className={`${galano.variable} font-galano antialiased`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript dangerouslySetInnerHTML={{
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M9DWRX7N"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+        }} />
+        {/* End Google Tag Manager (noscript) */}
+        
         <AuthProvider>
           <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
             {children}
           </ThemeProvider>
         </AuthProvider>
+        
+        {/* RD Station script */}
+        <Script 
+          id="rd-station-script"
+          type="text/javascript"
+          src="https://d335luupugsy2.cloudfront.net/js/loader-scripts/d21e392b-f626-4e80-892f-ca17bc6cc636-loader.js"
+          strategy="lazyOnload"
+          async
+        />
       </body>
     </html>
   )
